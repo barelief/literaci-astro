290c7198472c54f4ab2cae047b7cc289569507f0
Bartosh Polonski <barelief@users.noreply.github.com>
2023-02-07 19:29:43 +0200

    init

---
+++ astro.config.mjs
@@ -0,0 +1,19 @@
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import image from "@astrojs/image";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://literaci.lt",
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+  ],
+});
---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Stowarzyszenie Literatów Polskich na Litwie';
+export const SITE_DESCRIPTION = 'Literaci polscy na Litwie zrzeszają osoby działające w dziedzinie literatury, reprezentują ich interesy, promują międzynarodową współpracę literacką i kształtują atrakcyjny wizerunek polskiej literatury na Litwie i za granicą.';
